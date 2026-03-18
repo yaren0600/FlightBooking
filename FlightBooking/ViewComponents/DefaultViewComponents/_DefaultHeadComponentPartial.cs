8f80b3b18b200b8f3d4a18e49cc28c204364b242
yaren0600 <begumozturk0600@gmail.com>
2026-03-18 03:12:14 +0300

    Refactored the project structure by implementing modular View Components for reusable UI elements and optimizing the project configuration for better maintainability and code organization.

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() // This method is called when the view component is invoked in a view
+        {
+            return View();
+        }
+    }
+}
